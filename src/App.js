5ebcb4564557783684c83a89c4643dd67c8fa04e
Rashid <Lazyt3ch@gmail.com>
2021-02-27 19:54:30 +0500

    working on App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const stages = [
+    'Select Date and Tickets',
+    'Pay',
+    'Success'
+  ];
 
+  const [stageIndex, setStageIndex] = useState(0);
 
   return (
     <div className="App">
