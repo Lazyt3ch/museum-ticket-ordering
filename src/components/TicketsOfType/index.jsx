b1513976b6d2f7514103842250a58b66ef620add
Rashid <Lazyt3ch@gmail.com>
2021-02-27 20:49:52 +0500

    working on tickets of type

--- src/components/TicketsOfType/index.jsx
+++ src/components/TicketsOfType/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./index.css";
 
 function TicketsOfType(props) {
@@ -7,21 +7,29 @@ function TicketsOfType(props) {
     ticketPrice = "2 800 ₽",
     ticketDescription = "2 взрослых 1 ребёнок",
     isInfoButton = false,
-    infoText = "",
+    infoPopupText = "",
     contactPhone = "",
     isAddButton = false,
   } = props;
-  
+
+  const [isInfoButtonHovered, setIsInfoButtonHovered] = useState(false);  
 
   return (
     <div className="tickets">
       <div className="tickets__header">
         <div className="tickets__title">{ticketTitle}</div>
+        { isInfoButton && <button className="tickets__info-button">info</button> }
         <div className="tickets__price">{ticketPrice}</div>
       </div> 
       
       { ticketDescription.length && <div>{ticketDescription}</div> }
 
+      { isAddButton && <button className="tickets__add-button">Добавить</button> }
+      { contactPhone.length && <div className="tickets__contact-phone">{contactPhone}</div> }
+
+      { isInfoButton && isInfoButtonHovered 
+          && <div className="tickets__info-popup tickets__info-popup_active">{infoPopupText}</div> 
+      }
 
     </div>
   )
