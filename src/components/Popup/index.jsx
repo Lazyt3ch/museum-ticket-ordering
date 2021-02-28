16584bd504e7fac5c4aeb9ff869e7876541d08b9
Rashid <Lazyt3ch@gmail.com>
2021-02-28 14:11:34 +0500

    working on Popup

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -3,17 +3,10 @@ import "./index.css";
 // Reference: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
 
 function Popup(props) {
-  // eslint-disable-next-line
   const { infoPopupText } = props;
 
   const [isInfoPopupVisible, setIsInfoPopupVisible] = useState(false);
 
-  /*
-<div class="popup" onclick="myFunction()">Click me to toggle the popup!
-  <span class="popuptext" id="myPopup">A Simple Popup!</span>
-</div>
-
-  */
   const togglePopup = () => {
     setIsInfoPopupVisible((prev) => !prev);
   };
@@ -21,12 +14,10 @@ function Popup(props) {
   return (
     <>
       <div className="tickets__info-popup">
-        <div className="tickets__info-popup-container">
-          <div class={`tickets__info-popup-text ${isInfoPopupVisible 
-              ? "tickets__info-popup-text_show" : ""}`}>
-            {infoPopupText}
-          </div>         
-        </div>
+        <div class={`tickets__info-popup-text ${isInfoPopupVisible 
+            ? "tickets__info-popup-text_show" : ""}`}>
+          {infoPopupText}
+        </div>         
 
         <div className="tickets__info-button" onClick={togglePopup}></div>
       </div>
