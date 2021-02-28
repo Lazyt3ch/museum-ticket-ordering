74d1d1854a99da1d9fe989a6f5ce7b1d6b1d0c33
Rashid <Lazyt3ch@gmail.com>
2021-02-28 12:34:31 +0500

    working on Popup

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import "./index.css";
+// Reference: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
 
 function Popup(props) {
   const { infoPopupText } = props.data;
 
+  /*
+<div class="popup" onclick="myFunction()">Click me to toggle the popup!
+  <span class="popuptext" id="myPopup">A Simple Popup!</span>
+</div>
+
+  */
+
   return (
-    <div className="tickets__info-popup">
+    <div className="tickets__info-popup"
+    
+    >
 
-
+      
     </div>
   )
 }
