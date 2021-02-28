32d238b004db3e7cde285530d719b5e17a833b51
Rashid <Lazyt3ch@gmail.com>
2021-02-28 11:03:03 +0500

    working on DatePicker

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -1,7 +1,8 @@
-import React, {useState, useRef, useEffect} from 'react';
+// import React, {useState, useRef, useEffect} from 'react';
+import React from 'react';
 import "./index.css";
 
-import DayPicker from 'react-day-picker';
+// import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import DayPickerInput from "react-day-picker/DayPickerInput";
 
@@ -10,12 +11,16 @@ function DatePicker(props) {
     priceInfo = "Стоимость билета в праздничные дни рассчитывается по тарифу выходного дня",
   } = props;
 
-  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  // const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
-  const hiddenInput = useRef(null);
-  // HiddenInput.current.showDayPicker(); // .hideDayPicker();
-  const calendarOpener = hiddenInput;
-  console.dir("calendarOpener =", calendarOpener);
+  // console.log("DayPickerInput =", DayPickerInput);
+
+  // const hiddenInput = useRef(null);
+  // console.dir("hiddenInput =", hiddenInput);
+
+  // // HiddenInput.current.showDayPicker(); // .hideDayPicker();
+  // const calendarOpener = hiddenInput.current;
+  // console.dir("calendarOpener =", calendarOpener);
 
    // eslint-disable-next-line
   const datePickerStyles = {
@@ -23,44 +28,52 @@ function DatePicker(props) {
 
   };
 
-  const toggleCalendar = () => {
-    setIsCalendarOpen((prev) => !prev);
-  };
+  // const toggleCalendar = () => {
+  //   setIsCalendarOpen((prev) => !prev);
+  // };
 
-  useEffect(
-    () => {
-      isCalendarOpen
-        ? calendarOpener.showDayPicker()
-        : calendarOpener.hideDayPicker();
-    }, 
-    [isCalendarOpen, calendarOpener]
-  );
+  // useEffect(
+  //   () => {
+  //     isCalendarOpen
+  //       ? calendarOpener.showDayPicker()
+  //       : calendarOpener.hideDayPicker();
+  //   }, 
+  //   [isCalendarOpen, calendarOpener]
+  // );
 
   return (
     <div className="date-picker" >
       <div className="date-picker__calendar_control">
-        <div className="date-picker__hidden_input" >
-          <DayPickerInput ref={hiddenInput}></DayPickerInput>
-        </div>
+        <DayPickerInput 
+            placeholder="Выберите дату" format="DD.MM.YYYY"
+            // ref={hiddenInput} 
+        />
 
-        <div className="date-picker__opener"
+        {/* <div className="date-picker__hidden_input" >
+          <DayPickerInput 
+            placeholder="Выберите дату" format="DD.MM.YYYY"
+            ref={hiddenInput} 
+          />
+        </div> */}
+
+        {/* <div className="date-picker__opener"
           onClick={toggleCalendar}
         >
+
           <div className="date-picker__opener-text">
             Выберите дату
           </div>
 
-          <button className="date-picker__opener-button"
-                        
-          >            
-          </button>
-        </div>
+          <button className="date-picker__opener-button"></button>
+          
+        </div> */}
 
 
-        <DayPicker 
+        {/* <DayPicker 
           month={new Date()}
 
-        />
+        /> */}
+
       </div>
       
       <div className="date-picker__price-info">
