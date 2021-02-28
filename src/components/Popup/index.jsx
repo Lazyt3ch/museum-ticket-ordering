8e56834edb69ebf3c3c4bbc6c64b56439464c28f
Rashid <Lazyt3ch@gmail.com>
2021-02-28 12:19:39 +0500

    add Popup for Tickets

---
+++ src/components/Popup/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./index.css";
+
+function Popup(props) {
+  const { infoPopupText } = props.data;
+
+  return (
+    <div className="tickets__info-popup">
+
+
+    </div>
+  )
+}
+
+export default Popup;
