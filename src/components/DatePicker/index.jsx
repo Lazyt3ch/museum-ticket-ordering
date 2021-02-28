7d616696d52a0a042091c9330d218550a421d318
Rashid <Lazyt3ch@gmail.com>
2021-02-28 10:05:03 +0500

    working on DatePicker

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import "./index.css";
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 function DatePicker(props) {
   const {
     priceInfo = "Стоимость билета в праздничные дни рассчитывается по тарифу выходного дня",
   } = props;
 
+  const datePickerStyles = {
+
+
+  }
+
   return (
     <div className="date-picker">
-      SelectDate
       <div className="date-picker__calendar_control">
-        SelectDate
+        <div className="date-picker__opener">
+          <div className="date-picker__opener-text">
+            Выберите дату
+          </div>
+          <button className="date-picker__opener-button"
+                        
+          >
+            
+          </button>
+        </div>
+        <DayPicker 
+          month={new Date()}
+
+        />
       </div>
       
       <div className="date-picker__price-info">
