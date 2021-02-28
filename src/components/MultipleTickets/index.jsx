51d72f9454c26300eae14f7e6157e55948171407
Rashid <Lazyt3ch@gmail.com>
2021-02-28 17:08:42 +0500

    working on MultipleTickets

--- src/components/MultipleTickets/index.jsx
+++ src/components/MultipleTickets/index.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import "./index.css";
 
 function MultipleTickets(props) {
+  const {
+    numTickets,
+    setNumTickets,
+  } = props;
 
   return (
     <div className="multiple-tickets">
-      Multiple Tickets
+      <div className="multiple-tickets__remove-wrapper">
+        <button 
+          className={`multiple-tickets__remove-button`} 
+          disabled={!numTickets}
+          onClick={() => setNumTickets(0)}
+        >
+          Удалить
+        </button> 
+      </div>
+
+      <div className="multiple-tickets__increment-decrement-wrapper">
+        <button 
+          className={`multiple-tickets__decrement-button`} 
+          disabled={numTickets < 1}
+          onClick={() => setNumTickets((prev) => prev - 1)}
+        >
+          +
+        </button>      
+
+        <div className="multiple-tickets__subtotal">
+          numTickets
+        </div>
+
+        <button 
+          className={`multiple-tickets__increment-button`} 
+          // disabled={!numTickets}
+          onClick={() => setNumTickets((prev) => prev + 1)}
+        >
+          +
+        </button> 
+      </div>      
+
+
     </div>
   )
 }
