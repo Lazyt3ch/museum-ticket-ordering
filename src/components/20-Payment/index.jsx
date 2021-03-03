b55c7f2d8c9008e3fd37035d2a07cb2506211de7
Rashid <Lazyt3ch@gmail.com>
2021-03-03 16:37:32 +0500

    clean up Payment

--- src/components/20-Payment/index.jsx
+++ src/components/20-Payment/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useRef} from 'react';
 import "./index.css";
 import Checkbox from "../Checkbox";
 import Footer from "../Footer";
@@ -17,19 +17,8 @@ function Payment(props) {
     setEmail,
   } = props;
 
-  // eslint-disable-next-line
-  // const [email, setEmail] = useState("");
-
   const emailInputOne = useRef(null);
   const emailInputTwo = useRef(null);
-
-  // useEffect(
-  //   () => {
-
-
-  //   },
-  //   []
-  // )
 
   const verifyEmails = () => {
     // console.log("verify emails");
@@ -51,10 +40,6 @@ function Payment(props) {
 
   return (
     <div className="payment__container">
-      {/* <div className="payment__development-message">
-        Payment Stage: To be implemented
-      </div> */}
-
       <div className="payment__header">
         <div className="payment__header-text">Покупка билета</div>
         <div className="payment__header-description">
@@ -85,7 +70,8 @@ function Payment(props) {
           termsAccepted={termsAccepted}
           setTermsAccepted={setTermsAccepted}
         />
-
+        
+        {/* TODO: Replace both dummy links */}
         <div className="payment__accept-terms__text">
           Я соглашаюсь с <a className="payment__accept-terms__link" href="ticket-change-or-return-terms">Правилами обмена и возврата билетов</a> и <a className="payment__accept-terms__link" href="exhibition-visiting-terms">Правилами посещения Выставки</a>
         </div>
