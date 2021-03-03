2d4b3acc3c0631b38b664b3a042870f7d5835404
Rashid <Lazyt3ch@gmail.com>
2021-03-03 13:48:04 +0500

    working on Payment

--- src/components/20-Payment/index.jsx
+++ src/components/20-Payment/index.jsx
@@ -5,9 +5,16 @@ function Payment(props) {
 
   return (
     <div className="payment__container">
-      <div className="payment__development-message">
+      {/* <div className="payment__development-message">
         Payment Stage: To be implemented
-      </div>
+      </div> */}
+      <div className="payment__header">
+        <h1 className="payment__header-text">Покупка билета</h1>
+        <p className="payment__header-description">
+          Укажите вашу электронную почту для получения билета и чека
+        </p>
+      </div>    
+
     </div>
   )
 }
