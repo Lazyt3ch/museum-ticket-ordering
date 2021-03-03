c0e7807e9ec762219a844a2f49dd452a57b07792
Rashid <Lazyt3ch@gmail.com>
2021-03-03 15:25:49 +0500

    extract footer from DateAndTickets to Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./index.css";
+
+function Footer(props) {
+  const {
+    total, 
+    currencySign, 
+    buttonText,
+  } = props;       
+
+  return (
+    <div className="date-and-tickets__footer">
+      <div className="date-and-tickets__total">
+        Итого: {total} {currencySign}
+      </div>
+
+      <button 
+        className={`date-and-tickets__go-to-payment ${
+          total ? "date-and-tickets__go-to-payment_enabled" : ""}`}
+        disabled={!total}
+        onClick={() => setStageIndex((prev) => prev + 1)}
+      >
+        Перейти к оплате
+      </button>
+    </div>   
+  );
+}
+
+export default Footer;
