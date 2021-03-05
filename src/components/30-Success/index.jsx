adb522e97508782e99ef9a85774bd4034209b71b
Rashid <Lazyt3ch@gmail.com>
2021-03-05 12:19:39 +0500

    working on Success

--- src/components/30-Success/index.jsx
+++ src/components/30-Success/index.jsx
@@ -6,14 +6,18 @@ function Success(props) {
   return (
     <div className="success__container">
       <div className="success__nav">
-        <div className="success__nav__arrow">
-          
-        </div>
+        <div className="success__nav__arrow"></div>
         Вернуться на сайт
-
       </div>
 
-      Success (Stage 2)
+      <div className="success__info-card">
+        <div className="success__info-card__header">
+          <div className="success__info-card__header__checkmark"></div>
+          Покупка билетов прошла успешно
+        </div>
+      </div>
+
+      
     </div>
   )
 }
