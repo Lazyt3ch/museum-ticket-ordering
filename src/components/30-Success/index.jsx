a3634a0471512cfcf817dd1e156eacd4e0aa9df0
Rashid <Lazyt3ch@gmail.com>
2021-03-06 12:25:19 +0500

    simplify style names in Success

--- src/components/30-Success/index.jsx
+++ src/components/30-Success/index.jsx
@@ -16,91 +16,91 @@ function Success(props) {
   };
 
   return (
-    <div className="success__container">
-      <div className="success__nav">        
-        <div className="success__nav__arrow"
+    <div className="success">
+      <div className="success-nav">        
+        <div className="success-nav__arrow-background-image"
           onClick={goBack}
           tabIndex={0}
         >
-          <div className="success__nav__arrow__background-color"></div>
+          <div className="success-nav__arrow-background-color"></div>
         </div>
-        <div className="success__nav__text">
+        <div className="success-nav__text">
           Вернуться на сайт
         </div>
       </div>
 
-      <div className="success__info-card">
-        <div className="success__info-card__header">
-          <div className="success__info-card__header__checkmark">
-            <div className="success__info-card__header__checkmark__background-color"></div>
+      <div className="success-card">
+        <div className="success-card__header">
+          <div className="success-card__checkmark-background-image">
+            <div className="success-card__checkmark-background-color"></div>
           </div>
           <p>
             Покупка билетов прошла успешно
           </p>
         </div>
 
-        <div className="success__info-card__divider"></div>
+        <div className="success-card__divider"></div>
 
-        <div className="success__info-card__main">
-          <p className="success__info-card__main__info-type">
+        <div className="success-card__main">
+          <p className="success-card__info-type">
             ID: 000000001
           </p>
-          <p className="success__info-card__main__info-details">
+          <p className="success-card__info-details">
             Выставка “Body Worlds”
           </p>
-          <p className="success__info-card__main__info-type">
+          <p className="success-card__info-type">
             Дата посещения
           </p>
-          <p className="success__info-card__main__info-details">
+          <p className="success-card__info-details">
             {getFormatedDate(selectedDate)}
           </p>
         </div>        
 
-        <div className="success__info-card__divider"></div>
+        <div className="success-card__divider"></div>
 
-        <div className="success__info-card__footer">
-          <p className="success__info-card__footer__payment-title">
+        <div className="success-card__footer">
+          <p className="success-card__payment-title">
             Оплачено
           </p>
-          <div className="success__info-card__footer__payment-details">
-            <span className="success__info-card__footer__order-details__payment-total">
+          <div className="success-card__payment-details">
+            <span className="success-card__payment-total">
               {getFormatedPrice(total, currencySign)}
             </span>
-            <span className="success__info-card__footer__order-details__payment-method">
+            <span className="success-card__payment-method">
               Картой онлайн
             </span>
           </div>
-          <p className="success__info-card__footer__payment-info">
+          <p className="success-card__payment-info">
             Электронный билет и чек были отправлены на указанную Вами электронную почту
           </p>         
         </div>     
       </div>
 
-      <div className="success__exhibition-rules">
-        <div className="success__exhibition-rules__image"></div>
+      <div className="success-rules">
+        <div className="success-rules__mask-image"></div>
 
-        <div className="success__exhibition-rules__text">
-          <p className="success__exhibition-rules__title">
+        <div className="success-rules__text">
+          <p className="success-rules__title">
             требования по посещению выставки
           </p>
-          <p className="success__exhibition-rules__details">
+          <p className="success-rules__details">
             Необходимо использовать индивидуальные средства защиты (маски, перчатки) и соблюдать дистанцию 1,5 &mdash; 2 метра
           </p>          
         </div>
       </div>
       
-      <div className="success__contact-info">
-        <p className="success__contact-info__title">
+      <div className="success-contact">
+        <p className="success-contact__title">
           Отдел технической поддержки по электронным билетам
         </p>
 
-        <div className="success__contact-info__details">
-          <span className="success__contact-info__details__phone-icon"></span>
-          <span className="success__contact-info__details__text">
+        <div className="success-contact__details">
+          <span className="success-contact__phone-icon"></span>
+          <span className="success-contact__text">
             +7(499)643-82-38
           </span>          
-          <span className="success__contact-info__details__mail-icon"></span>
-          <span className="success__contact-info__details__text">
+          <span className="success-contact__mail-icon"></span>
+          <span className="success-contact__text">
             info@bodyworlds.moscow          
           </span>          
         </div>         
